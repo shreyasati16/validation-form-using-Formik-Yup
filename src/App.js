9225175d9614809cf4c2d2f5efe300ab8d95a592
Shreya <shreya.sati@kiwitech.com>
2023-01-03 12:18:44 +0530

    using formik

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Registration from './Registration';
+function App(){
+  const data= true;
+  return(
+    <div className="App">
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Registration.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { useFormik } from "formik";
+import { signUpSchema } from "./schemas/index";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+};
+
+const Registration = () => {
+  const {values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    validationSchema:signUpSchema,
+    onSubmit: (values,action) => {
+      console.log(values);
+      action.resetForm();
+    },
+  });
+  console.log(errors);
+
+  return (
+    <>
+     <div className="container">
+          <div className="modal">
+            <div className="modal-container">
+              <div className="modal-left">
+                <h1 className="modal-title">Welcome!</h1>
+                <p className="modal-desc">
+                  Validation Form
+                </p>
+                <form onSubmit={handleSubmit}>
+                  <div className="input-block">
+                    <label htmlFor="name" className="input-label">
+                      Name
+                    </label>
+                    <input
+                      type="name"
+                      autoComplete="off"
+                      name="name"
+                      id="name"
+                      placeholder="Name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.name && touched.name ? (
+                      <p className="form-error">{errors.name}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="email" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      autoComplete="off"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.email && touched.email ? (
+                      <p className="form-error">{errors.email}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password ? (
+                      <p className="form-error">{errors.password}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="confirm_password" className="input-label">
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="confirm_password"
+                      id="confirm_password"
+                      placeholder="Confirm Password"
+                      value={values.confirm_password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.confirm_password && touched.confirm_password ? (
+                      <p className="form-error">{errors.confirm_password}</p>
+                    ) : null}
+                  </div>
+                  <div className="modal-buttons">
+                    <a href="#" className="">
+                      Want to register using Gmail?
+                    </a>
+                    <button className="input-button" type="submit">
+                      Registration
+                    </button>
+                  </div>
+                </form>
+                <p className="sign-up">
+                  Already have an account? <a href="#">Sign In now</a>
+                </p>
+              </div>
+              <div className="modal-right">
+                <img
+                  src="https://cutshort.io/_next/image?url=https%3A%2F%2Fcdnv2.cutshort.io%2Fcompany-static%2F59a7da380adfdb705f965d8b%2Fuser_uploaded_data%2Fcover_pictures%2FKiwitech_Image_whTgqWd1.jpg&w=750&q=75"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default Registration;
